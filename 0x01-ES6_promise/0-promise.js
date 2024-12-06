7e4930fc70c0068112a52ccb8b2134f820e24a36
kachinne <nobbnet@gmail.com>
2024-12-06 08:45:21 -0800

    All is with God

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,9 +1,3 @@
-function getFullResponseFromAPI(apiEndpoint) {
-  return new Promise((resolve, reject) => {
-    if (apiEndpoint) resolve({ status: 200, body: 'Success' });
-    reject(Error('Failed to fetch API'));
-  });
+export default function getResponseFromAPI() {
+  return new Promise(() => {});
 }
-// Export the function for external use
-export default getFullResponseFromAPI;
-
