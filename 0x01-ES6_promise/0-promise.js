8c85573dcd10cb74e4cc060983266c98d1d06a4e
kachinne <nobbnet@gmail.com>
2024-12-06 09:00:05 -0800

    Yes we can

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,8 +1,7 @@
-function getFullResponseFromAPI(apiEndpoint) {
-  return new Promise((resolve, reject) => {
-    if (apiEndpoint) resolve({ status: 200, body: 'Success' });
-    reject(Error('Failed to fetch API'));
+export default function getResponseFromAPI() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('Success!');
+    }, 1000);
   });
 }
-// Export the function for external use
-export default getFullResponseFromAPI;
