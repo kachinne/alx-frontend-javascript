33ba038d08263481047e39afbc3d8ea5f3ad5c1d
kachinne <nobbnet@gmail.com>
2024-12-08 06:45:27 -0800

    No backing

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,9 +1,10 @@
-const updateUniqueItems = (map) => {
-  if (!(map instanceof Map)) throw new Error('Cannot process');
-
-  map.forEach((value, key) => {
-    if (value === 1) map.set(key, 100);
+export default function updateUniqueItems(oldMap) {
+  if (!(oldMap instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+  Array.from(oldMap.keys()).forEach((key) => {
+    if (oldMap.get(key) === 1) {
+      oldMap.set(key, 100);
+    }
   });
-};
-
-export default updateUniqueItems;
+}
